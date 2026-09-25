e8d328d53168dcea9d993217a8384b8dba91861f
Shivesh Joshi <shivesh.joshi.averybit@gmail.com>
2026-09-25 15:46:38 +0530

    Fixing base url

--- src/services/apiConstants.jsx
+++ src/services/apiConstants.jsx
@@ -1,7 +1,7 @@
-export const API_BASE_URL = 'http://localhost:5062/api';
-export const ASSETS_BASE_URL = 'http://localhost:5062';
-// export const API_BASE_URL = 'https://amplica.in/api';
-// export const ASSETS_BASE_URL = 'https://amplica.in';
+// export const API_BASE_URL = 'http://localhost:5062/api';
+// export const ASSETS_BASE_URL = 'http://localhost:5062';
+export const API_BASE_URL = 'https://amplica-backend.onrender.com/api';
+export const ASSETS_BASE_URL = 'https://amplica-backend.onrender.com';
 
 // Authentication Endpoints
 export const AUTH_ENDPOINTS = {
